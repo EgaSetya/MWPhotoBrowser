57c12f42414a021616ca500b9b8bb0f23197d1fa
Muhammad Nizar <nizar@ruangguru.com>
2017-05-30 18:46:03 +0700

    add love button

--- Pod/Classes/MWCaptionView.m
+++ Pod/Classes/MWCaptionView.m
@@ -15,7 +15,8 @@ static const CGFloat labelPadding = 10;
 // Private
 @interface MWCaptionView () {
     id <MWPhoto> _photo;
-    UILabel *_label;    
+    UILabel *_label;
+    UIButton *_loveButton;
 }
 @end
 
@@ -24,7 +25,7 @@ static const CGFloat labelPadding = 10;
 - (id)initWithPhoto:(id<MWPhoto>)photo {
     self = [super initWithFrame:CGRectMake(0, 0, 320, 44)]; // Random initial frame
     if (self) {
-        self.userInteractionEnabled = NO;
+        self.userInteractionEnabled = YES;
         _photo = photo;
         self.barStyle = UIBarStyleBlackTranslucent;
         self.tintColor = nil;
@@ -33,8 +34,27 @@ static const CGFloat labelPadding = 10;
         [self setBackgroundImage:nil forToolbarPosition:UIBarPositionAny barMetrics:UIBarMetricsDefault];
         self.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleRightMargin;
         [self setupCaption];
+        [self setupLoveButton];
     }
     return self;
+}
+
+- (void)setupLoveButton {
+    // add love button
+    _loveButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [_loveButton setImage:[UIImage imageNamed:@"loveNormalSummary"] forState:UIControlStateNormal];
+    [_loveButton setTitle:@"30" forState:UIControlStateNormal];
+    [_loveButton.titleLabel setFont:[UIFont fontWithName:@"Helvetica Neue" size:12]];
+    [_loveButton setImage:[UIImage imageNamed:@"loveNormalSummary"] forState:UIControlStateHighlighted];
+    [_loveButton addTarget:self action:@selector(loveButtonDidTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [_loveButton sizeToFit];
+    _loveButton.titleEdgeInsets = UIEdgeInsetsMake(0, 5, 0, 0);
+    _loveButton.frame = CGRectMake(self.bounds.size.width - 10, self.bounds.size.height/2 - 20,
+                                   60,
+                                   40);
+    UIBarButtonItem *flexible = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:self action:nil];
+    UIBarButtonItem* loveBarButtonItem =[[UIBarButtonItem alloc]initWithCustomView:_loveButton];
+    self.items = [NSArray arrayWithObjects:flexible,loveBarButtonItem, nil];
 }
 
 - (CGSize)sizeThatFits:(CGSize)size {
@@ -49,14 +69,14 @@ static const CGFloat labelPadding = 10;
 
 - (void)setupCaption {
     _label = [[UILabel alloc] initWithFrame:CGRectIntegral(CGRectMake(labelPadding, 0,
-                                                       self.bounds.size.width-labelPadding*2,
-                                                       self.bounds.size.height))];
+                                                                      self.bounds.size.width-labelPadding*2 - 50,
+                                                                      self.bounds.size.height))];
     _label.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
     _label.opaque = NO;
     _label.backgroundColor = [UIColor clearColor];
     _label.textAlignment = NSTextAlignmentLeft;
     _label.lineBreakMode = NSLineBreakByWordWrapping;
-
+    
     _label.numberOfLines = 0;
     _label.textColor = [UIColor whiteColor];
     _label.font = [UIFont fontWithName:@"Helvetica Neue" size:14];
@@ -64,6 +84,10 @@ static const CGFloat labelPadding = 10;
         _label.text = [_photo caption] ? [_photo caption] : @" ";
     }
     [self addSubview:_label];
+}
+
+- (void)loveButtonDidTapped:(id)sender {
+    NSLog(@"loveButtonDidTapped");
 }
 
 
